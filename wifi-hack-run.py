da4155a89e2671a1c0338a62b6c51d2ad66b8522
ninjahere786 <ilovepython662@gmail.com>
2024-08-29 04:20:47 -0700

    Add files via upload

---
+++ wifi-hack-run.py
@@ -0,0 +1,54 @@
+import time
+
+print("""
+██╗    ██╗██╗███████╗██╗    ██╗  ██╗ █████╗  ██████╗██╗  ██╗
+██║    ██║██║██╔════╝██║    ██║  ██║██╔══██╗██╔════╝██║ ██╔╝
+██║ █╗ ██║██║█████╗  ██║    ███████║███████║██║     █████╔╝ 
+██║███╗██║██║██╔══╝  ██║    ██╔══██║██╔══██║██║     ██╔═██╗ 
+╚███╔███╔╝██║██║     ██║    ██║  ██║██║  ██║╚██████╗██║  ██╗
+ ╚══╝╚══╝ ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+                                                            
+""")
+
+time.sleep(2)
+
+print('WELCOME TO WIFI HACK!')
+
+a = input("PLEASE ENTER 'Y' TO AGREE TO THE TERMS AND CONDITIONS>>>")
+
+if a == 'Y' or 'y':
+    pass
+
+else:
+    exit()
+    
+
+
+
+xa = input("What type of attack you want to Do?")
+
+
+if xa == "bruteForce":
+    pass
+
+else:
+    print("Please try again by exting :(")
+    
+    
+    
+
+b = input("Please enter the IP of your device")
+print("done!")
+time.sleep(1)
+
+n = input('please enter the name of the wifi device you want to connect to: ')
+print('Checking if Your Wifi password is strong or not!!....')
+time.sleep(5)
+print('Oh no!!!, this wifi has a really strong password and it will approximately take longer')
+print("please wait while you are connected!")
+time.sleep(1)
+
+print("it will take 15 minutes to Connect And Hack!!!")
+time.sleep(900)
+
+print(f"WIFI Name = {n}\n WIFI Password: 42507160")
